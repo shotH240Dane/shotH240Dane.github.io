ffb27ef34cdc0a197bf5c5eb30f5d4f1fdee27e5
Dane <2401757537@qq.com>
2022-08-18 16:18:57 +0800

    feature: create next pwa project

---
+++ next-demo/next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
+const withNextPlugins = require('next-compose-plugins')
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   pwa: {
+//     dest: 'public'
+//   }
+// }
+// console.log('nextConfig===================================', withNextPlugins([withPWA], nextConfig))
+// module.exports = withNextPlugins([withPWA], nextConfig)
+const nextConfig = {
+  distDir: 'build',
+  // reactStrictMode: true,
+  pwa: {
+    dest: 'public'
+  },
+  webpack: (config, options) => {
+    return config
+  }
+}
+
+module.exports = withNextPlugins([withPWA], nextConfig)
