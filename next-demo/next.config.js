b579853033cb75c98097fb250f29b2009c99f5d6
dane <2401757537@qq.com>
2022-08-19 17:46:19 +0800

    fix: set next-pwa service-worker

--- next-demo/next.config.js
+++ next-demo/next.config.js
@@ -1,21 +1,12 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')
 const withNextPlugins = require('next-compose-plugins')
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   pwa: {
-//     dest: 'public'
-//   }
-// }
-// console.log('nextConfig===================================', withNextPlugins([withPWA], nextConfig))
-// module.exports = withNextPlugins([withPWA], nextConfig)
 const nextConfig = {
   distDir: 'build',
   // reactStrictMode: true,
   pwa: {
     dest: 'public',
-    swSrc: 'service-worker.js',
+    swSrc: 'service-worker.js'
   },
   webpack: (config, options) => {
     return config
