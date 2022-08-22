9dc2db94ea69dc98e377ac30039897dde706dd1f
dane <2401757537@qq.com>
2022-08-22 14:23:28 +0800

    feature: 添加sass

--- next-demo/pages/About/index.tsx
+++ next-demo/pages/About/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react'
-
+import style from './css/index.module.scss'
 export default function About() {
   return (
-    <div>about</div>
+    <div className={style.main}>
+      <div className={style.box1}></div>
+      <div className={style.box2}></div>
+    </div>
   )
 }
