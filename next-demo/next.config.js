bb5d3c2fd6a28a55aa73a69066dfa9dbe041ad74
dane <2401757537@qq.com>
2022-08-22 14:40:18 +0800

    feature: 添加环境变量

--- next-demo/next.config.js
+++ next-demo/next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')
 const withNextPlugins = require('next-compose-plugins')
+console.log('1111111111111111111===', process.env)
 const nextConfig = {
   distDir: 'build',
   // reactStrictMode: true,
+  env: {
+    // NEXT_PUBLIC_NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV
+  },
   pwa: {
     dest: 'public',
     swSrc: 'service-worker.js'
--- next-demo/pages/About/index.tsx
+++ next-demo/pages/About/index.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import style from './css/index.module.scss'
 export default function About() {
+  useEffect(() => {
+    console.log('==', process.env, process.env.NEXT_PUBLIC_NODE_ENV)
+  }, [])
   return (
     <div className={style.main}>
       <div className={style.box1}></div>
